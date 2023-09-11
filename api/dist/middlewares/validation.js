f952063ae83ba1f0843bbdd1d155440635e78550
Santiago L. Romero <santiago_pna@hotmail.com>
2023-09-11 20:51:49 -0300

    Initial files v1.0.0

--- api/dist/middlewares/validation.js
+++ api/dist/middlewares/validation.js
@@ -3,12 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.validate = void 0;
 const errors_1 = require("../utils/errors");
 function validate(req, res, next) {
-    const { name } = req.body;
-    if (name) {
+    const { name, launch, rating, description, genres, platforms, img } = req.body;
+    if (name && launch && rating && description && genres && platforms && img) {
         next();
     }
     else {
-        throw new errors_1.ClientError(401, `Property name is missing`);
+        throw new errors_1.ClientError(401, `Some properties are missing. Check name, launch, rating, description, genres, platforms or img`);
     }
 }
 exports.validate = validate;
--- api/src/middlewares/validation.ts
+++ api/src/middlewares/validation.ts
@@ -2,11 +2,11 @@ import { Request, Response, NextFunction } from "express";
 import { ClientError } from "../utils/errors";
 
 export function validate (req: Request, res: Response, next: NextFunction) {
-    const { name } = req.body;
-    if (name) {
+    const { name, launch, rating, description, genres, platforms, img } = req.body;
+    if (name && launch && rating && description && genres && platforms && img) {
         next();
     }
     else {
-        throw new ClientError(401, `Property name is missing`);
+        throw new ClientError(401, `Some properties are missing. Check name, launch, rating, description, genres, platforms or img`);
     }
 }
